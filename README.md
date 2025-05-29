2e127d7cb2bdf1e54f7b155552652c173577017c
kavitagupta972 <kavitagupta972@gmail.com>
2025-05-29 13:14:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# progresbar-react
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kavitagupta972/progresbar-react)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+import ProgressBar from './ProgressBar';
+
+export default function App() {
+  return (
+    <div>
+      <ProgressBar />
+    </div>
+  );
+}
